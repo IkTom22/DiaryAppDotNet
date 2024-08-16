0a10bcb31e6ecb92ec645a763f11bebb0c9ffc10
Iko <ikotominaga@gmail.com>
2024-08-17 11:46:52 +1200

    deeper error message like string length is added

--- Models/DiaryEntryModel.cs
+++ Models/DiaryEntryModel.cs
@@ -7,12 +7,13 @@ public class DiaryEntry
     // [Key]
     public int Id {get; set;}
     // ? creates nullable
-    [Required]
+    [Required(ErrorMessage ="Please enter a title!")]
+    [StringLength(100, MinimumLength =3, 
+        ErrorMessage ="Title must be between 3 and 100 characters!")]
     public string Title {get; set;} = string.Empty;
     // = string.Empty initialising by default value 
-    [Required]
+    [Required(ErrorMessage ="Please enter content")]
     public string Content {get; set;} = string.Empty;
-    [Required]
+    [Required(ErrorMessage = "Please enter date")]
     public DateTime Created {get; set;} = DateTime.Now;
-
 }
